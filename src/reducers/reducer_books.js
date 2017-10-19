91b2b4e47be1ba57fec187258930bcb8c3bbd1ec
bambery <lwszolek@gmail.com>
2017-10-18 22:35:15 -0700

    P02L40 booklist component & reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: "Use of Weapons" },
+    { title: "Nausea" },
+    { title: "Consider Phlebas" },
+    { title: "Lady Chatterly's Lover" }
+  ];
+}
